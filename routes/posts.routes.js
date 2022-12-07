7c9280895292e2b92eb7b640c22958cdc4e330dd
Catalina Sabogal <csabogalortiz@gmail.com>
2022-12-07 16:43:31 +0100

    Private ROutes Check

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const { findByIdAndUpdate, findByIdAndDelete } = require("../models/Post.model")
 const Post = require('../models/Post.model')
 const User = require('../models/User.model')
+const { isAuthenticated } = require("../midleware/jwt.middleware")
+
+
 
 
 // Get All Posts
@@ -27,22 +30,23 @@ router.get("/:post_id", (req, res, next) => {
 
 // Create Post
 
-router.post('/create', (req, res, next) => {
+router.post('/create', isAuthenticated, (req, res, next) => {
     const { title, content } = req.body
     let prueba
     let user
-    // const { _id: owner } = req.session.currentpost
+
 
     Post
-        .create({ title, content })
-        .then(response => prueba = response)
-
-
-    return User
-        //   let whole: {prueba, user}
-        .findByIdAndUpdate(user_id, { "$push": { "comments": comment._id } })
+        .create({ ...req.body, owner: req.payload_id })
         .then(response => res.json(response))
         .catch(err => res.status(500).json(err))
+
+
+    // return User
+    //     //   let whole: {prueba, user}
+    //     .findByIdAndUpdate(user_id, { "$push": { "comments": comment._id } })
+    //     .then(response => res.json(response))
+    //     .catch(err => res.status(500).json(err))
 })
 
 
